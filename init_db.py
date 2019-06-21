0d503d8b71bcf8bc455a97fbe1ebbd132101031c
tw1666@nyu.edu <6Cantabile6>
2019-06-21 15:29:15 +0800

    init added

---
+++ init_db.py
@@ -0,0 +1,223 @@
+# Import Flask Library
+from flask import Flask, render_template, request, session, url_for, redirect
+import pymysql.cursors
+
+# Initialize the app from Flask
+app = Flask(__name__)
+
+# Configure MySQL
+conn = pymysql.connect(host='localhost',
+                       port=8889,
+                       user='root',
+                       password='root',
+                       db='blog',
+                       charset='utf8mb4',
+                       cursorclass=pymysql.cursors.DictCursor)
+
+
+'
+
+@app.route('/')
+def public():
+    #publc info
+    return render_template('index.html')
+
+
+# Define route for login
+
+@app.route('/login')
+def login():
+	return render_template('login.html')
+#login page, choose staff or customer
+
+@app.route('/login_staff')
+def login_staff():
+    return render_template('login-staff.html')
+
+@app.route('/login_customer')
+def login_customer():
+    return render_template('login-customer.html')
+
+# Define route for register
+@app.route('/register')
+def register():
+	return render_template('register.html')
+
+@app.route('/register_staff')
+def register_staff():
+    return render_template('register-staff.html')
+
+@app.route('/register_customer')
+def register_customer():
+    return render_template('register-customer.html')
+
+
+# Authenticates the login
+@app.route("/loginStaffAuth", methods=['GET', 'POST'])
+def loginStaffAuth():
+    # grabs information from the forms
+    username = request.form['username']
+    password = request.form['password']
+
+    # cursor used to send queries
+    cursor = conn.cursor()
+    # executes query
+    query = 'SELECT * FROM staff WHERE username = %s and password = %s'
+    cursor.execute(query, (username, password))
+    # stores the results in a variable
+    data = cursor.fetchone()
+    # use fetchall() if you are expecting more than 1 data row
+    cursor.close()
+    error = None
+    if (data):
+        # creates a session for the the user
+        # session is a built in
+        session['username'] = username
+        return redirect(url_for('home'))
+    else:
+        # returns an error message to the html page
+        error = 'Invalid login or username'
+        return render_template('login-staff.html', error=error)  # send the error msg to html
+    # communicate between python and html
+
+@app.route('/loginCustomerAuth', methods=['GET', 'POST'])
+def loginCustomerAuth():
+    # grabs information from the forms
+    username = request.form['username']
+    password = request.form['password']
+
+    # cursor used to send queries
+    cursor = conn.cursor()
+    # executes query
+    query = 'SELECT * FROM customer WHERE username = %s and password = %s'
+    cursor.execute(query, (username, password))
+    # stores the results in a variable
+    data = cursor.fetchone()
+    # use fetchall() if you are expecting more than 1 data row
+    cursor.close()
+    error = None
+    if (data):
+        # creates a session for the the user
+        # session is a built in
+        session['username'] = username
+        return redirect(url_for('home'))
+    else:
+        # returns an error message to the html page
+        error = 'Invalid login or username'
+        return render_template('login-customer.html', error=error)  # send the error msg to html
+    # communicate between python and html
+
+
+# Authenticates the register
+@app.route("/registerStaffAuth", methods=['GET', 'POST'])
+def registerStaffAuth():
+    # grabs information from the forms
+    username = request.form['username']
+    airline_name = request.form['airline-name']
+    password = request.form['password']
+    first_name = request.form['first-name']
+    last_name = request.form['last-name']
+    DOB = request.form['DOB']
+    phone_number = request.form['phone-number']
+
+    # cursor used to send queries
+    cursor = conn.cursor()
+    # executes query
+    query = 'SELECT * FROM staff WHERE username = %s'
+    cursor.execute(query, (username))
+    # stores the results in a variable
+    data = cursor.fetchone()
+    # use fetchall() if you are expecting more than 1 data row
+    error = None
+    if (data):
+        # If the previous query returns data, then user exists
+        error = "This user already exists"
+        return render_template('register-staff.html', error=error)
+    else:
+        ins = 'INSERT INTO staff VALUES(%s, %s, %s, %s, %s, %s, %s)'
+        cursor.execute(ins, (username, airline_name, password,
+                             first_name, last_name, DOB, phone_number))
+        conn.commit()
+        cursor.close()
+        #todo: redirect to staff login page?
+        return render_template('login-staff.html')
+
+@app.route("/registerCustomerAuth", methods=['GET', 'POST'])
+def registerCustomerAuth():
+    # grabs information from the forms
+    email = request.form['email']
+    password = request.form['password']
+    name = request.form['name']
+    DOB = request.form['DOB']
+    phone_number = request.form['phone-number']
+    building_number = request.form['building_number']
+    street = request.form['street']
+    city = request.form['city']
+    state = request.form['state']
+    passport_number = request.form['passport-number']
+    passport_expiration = request.form['passport-expiration']
+    passport_country = request.form['passport-country']
+
+    # cursor used to send queries
+    cursor = conn.cursor()
+    # executes query
+    query = 'SELECT * FROM customer WHERE email = %s'
+    cursor.execute(query, (email))
+    # stores the results in a variable
+    data = cursor.fetchone()
+    # use fetchall() if you are expecting more than 1 data row
+    error = None
+    if (data):
+        # If the previous query returns data, then user exists
+        error = "This user already exists"
+        return render_template('register.html', error=error)
+    else:
+        ins = 'INSERT INTO customer VALUES' \
+              '(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+        cursor.execute(ins, (email, password, name, DOB, phone_number,
+                             building_number, street, city, state,
+                             passport_number, passport_expiration, passport_country))
+        conn.commit()
+        cursor.close()
+        #todo: redirect to customer login page?
+        return render_template('login-customer.html')
+
+#=========below I did not edit==========================
+
+@app.route('/home')
+def home():
+    username = session['username']
+    cursor = conn.cursor();
+    query = 'SELECT ts, blog_post FROM blog WHERE username = %s ORDER BY ts DESC'
+    cursor.execute(query, (username))
+    data1 = cursor.fetchall()
+    for each in data1:
+        print(each['blog_post'])
+    cursor.close()
+    return render_template('home.html', username=username, posts=data1)
+
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    username = session['username']
+    cursor = conn.cursor();
+    blog = request.form['blog']
+    query = 'INSERT INTO blog (blog_post, username) VALUES(%s, %s)'
+    cursor.execute(query, (blog, username))
+    conn.commit()
+    cursor.close()
+    return redirect(url_for('home'))
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username')
+    return redirect('/')
+#todo: goodbye page
+
+app.secret_key = 'some key that you will never guess'
+# Run the app on localhost port 5000
+# debug = True -> you don't have to restart flask
+# for changes to go through, TURN OFF FOR PRODUCTION
+if __name__ == "__main__":
+    app.run('127.0.0.1', 5000, debug=True)
