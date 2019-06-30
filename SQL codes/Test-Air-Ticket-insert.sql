6fa5ccdc7fc200c272194652cc6b46dfe5732f1a
tw1666@nyu.edu <6Cantabile6>
2019-06-30 17:16:21 +0800

    expiration date

--- SQL codes/Test-Air-Ticket-insert.sql
+++ SQL codes/Test-Air-Ticket-insert.sql
@@ -1,2 +1,31 @@
 INSERT INTO airline VALUES ('Air China');
 
+create table purchase
+	(ticket_id varchar(50),
+	email varchar(50),
+	purchase_date date,
+	purchase_time time,
+	sold_price float(2) not null,
+	card_type varchar(50),
+	card_number varchar(50),
+	name_on_card varchar(50),
+	expiraton_date date,
+	primary key (ticket_id, email),
+	foreign key (ticket_id) references ticket(ticket_id)
+		on update cascade,
+	foreign key (email) references customer(email)
+		on update cascade
+	);
+
+INSERT INTO purchase VALUES ('1', 'testcustomer@nyu.edu', '2019-03-12', '11:55:55', '300', 'credit card', '111-222-333-444', 'Test Customer 1', '2026-01-01');
+
+INSERT INTO purchase VALUES ('a00000001', 'js123@outlook.com', '2019-05-12', '00:10:10', '4000', 'credit card', '600080009000111', 'John Smith', '2026-01-01');
+INSERT INTO purchase VALUES ('a00000001', 'js123@outlook.com', '2019-05-12', '00:10:10', '4000', 'credit card', '600080009000111', 'John Smith', '2026-01-01');
+INSERT INTO purchase VALUES ('a00000001', 'js123@outlook.com', '2019-05-12', '00:10:10', '4000', 'credit card', '600080009000111', 'John Smith', '2026-01-01');
+INSERT INTO purchase VALUES ('a00000001', 'js123@outlook.com', '2019-05-12', '00:10:10', '4000', 'credit card', '600080009000111', 'John Smith', '2026-01-01');
+INSERT INTO purchase VALUES ('a00000001', 'js123@outlook.com', '2019-05-12', '00:10:10', '4000', 'credit card', '600080009000111', 'John Smith', '2026-01-01');
+INSERT INTO purchase VALUES ('a00000001', 'js123@outlook.com', '2019-05-12', '00:10:10', '4000', 'credit card', '600080009000111', 'John Smith', '2026-01-01');
+INSERT INTO purchase VALUES ('a00000001', 'js123@outlook.com', '2019-05-12', '00:10:10', '4000', 'credit card', '600080009000111', 'John Smith', '2026-01-01');
+INSERT INTO purchase VALUES ('a00000001', 'js123@outlook.com', '2019-05-12', '00:10:10', '4000', 'credit card', '600080009000111', 'John Smith', '2026-01-01');
+INSERT INTO purchase VALUES ('a00000001', 'js123@outlook.com', '2019-05-12', '00:10:10', '4000', 'credit card', '600080009000111', 'John Smith', '2026-01-01');
+INSERT INTO purchase VALUES ('a00000001', 'js123@outlook.com', '2019-05-12', '00:10:10', '4000', 'credit card', '600080009000111', 'John Smith', '2026-01-01');
--- init_db.py
+++ init_db.py
@@ -13,7 +13,7 @@ conn = pymysql.connect(host='localhost',
                        port=8889,
                        user='root',
                        password='root',
-                       db='Air-Ticket',
+                       db='Test-Air-Ticket',
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor)
 
@@ -155,7 +155,6 @@ def registerCustomerAuth():
                              passport_country, DOB))
         conn.commit()
         cursor.close()
-        #todo: redirect to customer login page?
         return render_template('login-customer.html')
 
 
@@ -839,11 +838,13 @@ def payCustomer():
     card_number = request.form['card-number']
     name_on_card = request.form['name-on-card']
     card_expiration = request.form['card-expiration']
+    print("expiration date", card_expiration, type(card_expiration))
     cursor = conn.cursor()
     ins = '''insert into purchase
     (ticket_id, email, purchase_date, purchase_time, sold_price, card_type, card_number, name_on_card, expiraton_date)
     values (%s, %s, cast(now() as date), cast(now() as time), %s, %s, %s, %s, %s)
     '''
+
     cursor.execute(ins, (flight_info1["ticket_id"], username, flight_info1["price"],
                            card_type, card_number, name_on_card, card_expiration))
     conn.commit()
